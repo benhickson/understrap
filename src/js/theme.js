def90e1c1441cf02c26bd5c52bc4b41a16a8565d
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2019-12-24 18:18:53 +0100

    New gulp workflow
    * Create .babelrc
    * Update gulpconfig.json
    * Create gulpfile.babel.js
    * Delete gulpfile.js
    * Update package.json
    * Move sass/ and src/sass/ to src/scss/
    * Update paths in src/scss/bootstrap4.scss
    * Update paths in src/scss/font-awesome.scss
    * Update paths in src/scss/theme.scss
    * Update path in src/scss/underscores.scss
    * Create src/js/components/component.js
    * Create src/js/include/custom.js
    * Create src/js/custom-scripts.js
    * Create empty src/images/
    * Delete css/theme.min.css.map
    * Delete css/theme.min.css
    * Delete css/theme.css
    * Delete css/custom-editor-style.min.css.map
    * Delete css/custom-editor-style.min.css
    * Delete css/custom-editor-style.css
    * Delete js/theme.js
    * Delete js/theme.min.js
    * Move js/customizer.js to src/js/customizer.js

---
+++ src/js/theme.js
@@ -0,0 +1,10 @@
+import './bootstrap4/bootstrap.min.js';
+
+// Here you can import custom scripts
+import './custom-scripts.js';
+
+// Here you can import custom reusable components
+import './components/component.js';
+
+
+console.log('THEME.JS LOADED');
